70942cd07d1fc87263b2e9a624b6ebcb699bcaf8
Jeremy <thepoweroverwhelming@gmail.com>
2024-01-24 11:03:48 -0500

    mock update call and route back to home page until domains are set

--- components/SaveItem.tsx
+++ components/SaveItem.tsx
@@ -3,6 +3,7 @@ import { Button, Grid, Stack, ToggleButton, ToggleButtonGroup, Typography } from
 import { useState } from "react";
 import axios from "axios";
 import { ItemState } from "../pages/index";
+import { useRouter } from "next/router";
 
 
 let bucketType = ["loss", "process", "donation", "Shoulder Tap"];
@@ -13,26 +14,29 @@ let reasonType = ["blemished", "unsightly", "no label", "close expiry"];
 
 export default function SaveItem(props: { itemState: ItemState }) {
     let [itemState, setItemState] = useState<ItemState>(props.itemState)
+    const router = useRouter()
     const headers = {
         'Content-Type': 'application/json',
         'X-Requested-With': null,
         'Access-Control-Allow-Origin': '*'
     }
     const postItem = async (itemState: ItemState) => {
-        const response = await axios.post("https://8e8oow3g70.execute-api.us-east-1.amazonaws.com/dev/lpds",
-            itemState,
-            { headers: headers });
-        if (response.status == 202) {
-            setItemState(state => {
-                return {
-                    id: undefined,
-                    bucketType: undefined,
-                    itemType: undefined,
-                    reasonType: undefined
-                }
-            })
-        }
-        return response.status;
+        
+        router.push('/')
+        // const response = await axios.post("https://8e8oow3g70.execute-api.us-east-1.amazonaws.com/dev/lpds",
+        //     itemState,
+        //     { headers: headers });
+        // if (response.status == 202) {
+        //     setItemState(state => {
+        //         return {
+        //             id: undefined,
+        //             bucketType: undefined,
+        //             itemType: undefined,
+        //             reasonType: undefined
+        //         }
+        //     })
+        // }
+        // return response.status;
     };
 
     const submitData = () => {
